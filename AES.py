4930e476bc0f1e205cafd7d49813982990dbe130
mregirl <53383902+mregirl@users.noreply.github.com>
2024-04-25 22:58:25 -0500

    Update AES.py

--- AES.py
+++ AES.py
@@ -2,15 +2,16 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 from secrets import token_bytes
 
-# Generate a pseudo-random key with a size of 16 bytes
-key=token_bytes(16)
-
 class AESinECB():
     def __init__(self):
-        self.aes=AES.new(key,AES.MODE_ECB)
+        # Generate a pseudo-random key with a size of 16 bytes
+        self.key=token_bytes(16)
+        self.aes=AES.new(self.key,AES.MODE_ECB)
     def encrypt(self,msg):
         padded_msg=pad(msg.encode("utf-8"),16) # need to do padding since AES block size is 16 bytes
         ciphertxt=self.aes.encrypt(padded_msg)
         return ciphertxt
     def decrypt(self, cipher):
         return self.aes.decrypt(cipher)
+    def getKey(self):
+        return self.key
